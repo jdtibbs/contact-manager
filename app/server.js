af4c1aed1e2a491a86604f90bb89bc4c7b0c6d44
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-28 09:38:01 -0600

    init riot setup

---
+++ app/server.js
@@ -0,0 +1,66 @@
+// see: 
+// https://nodejs.org/dist/latest-v4.x/docs/api/
+(function() {
+	var http = require('http');
+	var path = require('path');
+	var fs = require('fs');
+	var port = process.env.PORT || 8080;
+
+	var mimeTypes = {
+		'.css': 'text/css',
+		'.htlm': 'text/html',
+		'.js': 'text/javascript'
+	};
+
+	var urlFileMap = {
+		'/': '/index.html',
+	};
+
+	// to verify environment setting.
+	// console.log('env: ' + process.env.NODE_ENV);
+	// console.log('current working directory: ' + process.cwd());
+
+	http.createServer(function(request, response) {
+		var name = urlFileMap[request.url] || request.url;
+		getFile(name, response);
+	}).listen(parseInt(port, 10));
+
+	console.log('Server running at http://localhost:' + port);
+
+	function getFile(name, response) {
+		var fileName = 'app' + name;
+		fs.stat(fileName, function(error, stats) {
+			if (error) {
+				console.log('fs.stat error: ' + error + '\n');
+				response.writeHead(500, {
+					'Content-Type': 'text/plain'
+				});
+				response.write(error + '\n');
+				response.end();
+			} else if (!stats.isFile()) {
+				response.writeHead(404, {
+					'Content-Type': 'text/plain'
+				});
+				response.write('404 Not Found\n');
+				response.end();
+			} else {
+				fs.readFile(fileName, function(error, file) {
+					if (error) {
+						console.log('fs.readFile error: ' + error + '\n');
+						response.writeHead(500, {
+							'Content-Type': 'text/plain'
+						});
+						response.write(error + '\n');
+						response.end();
+					} else {
+						header = {
+							'Content-Type': mimeTypes[path.extname(name)]
+						};
+						response.writeHead(200, header);
+						response.end(file);
+					}
+				});
+			}
+		});
+	}
+})();
